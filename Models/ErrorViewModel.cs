d3350d6e2957293377ecef68d21a5713d23b839b
Ace Baugh <ace.d.baugh@gmail.com>
2024-05-13 13:05:08 -0400

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WDWShiftX.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
